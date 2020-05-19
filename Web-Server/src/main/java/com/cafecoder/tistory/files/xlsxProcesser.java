b33431011b1b0d47245123431518d060cdb0ccd9
hwk0911 <hwk0911@gmail.com>
2020-05-19 19:30:10 +0900

    POI 의존성 추가

--- Web-Server/src/main/java/com/cafecoder/tistory/files/xlsxProcesser.java
+++ Web-Server/src/main/java/com/cafecoder/tistory/files/xlsxProcesser.java
@@ -1,5 +1,45 @@
 package com.cafecoder.tistory.files;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
 public class xlsxProcesser {
-    //todo : 라이브러리 추가 및 함수 작성
+    private List<FileInputStream> fileInputStreams;
+    private List<HSSFWorkbook> workbooks;
+
+    public xlsxProcesser (List<File> files) {
+        this.fileInputStreams = new ArrayList<>();
+        this.workbooks = new ArrayList<>();
+
+        for(File file : files) {
+            try {
+                this.fileInputStreams.add(new FileInputStream(file));
+            }
+            catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        for(FileInputStream fis : fileInputStreams) {
+            try {
+                this.workbooks.add(new HSSFWorkbook(fis));
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void readXlsx () {
+
+    }
 }
