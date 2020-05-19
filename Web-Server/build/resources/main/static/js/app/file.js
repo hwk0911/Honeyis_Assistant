814505be958ed26b08fef4e727b2003aa6e5af56
hwk0911 <hwk0911@gmail.com>
2020-05-19 19:01:30 +0900

    Gradle 수정

--- Web-Server/build/resources/main/static/js/app/file.js
+++ Web-Server/build/resources/main/static/js/app/file.js
@@ -27,66 +27,30 @@ function uploadFiles(e) {
     e.dataTransfer = e.originalEvent.dataTransfer; //2
     var files = e.target.files || e.dataTransfer.files;
 
-    var data = new FormData(files);
+    alert(files[0].name);
 
-    alert('asdfadsfads');
+    var formData = new FormData();
+
+    for (var index = 0, size = files.length; index < size; ++index) {
+        formData.append("files", files[index]);
+    }
 
     $.ajax({
         type: "POST",
-            enctype: 'multipart/form-data',
-            url: "/api/v1/filesup",
-            data: data,
-            processData: false,
-            contentType: false,
-            cache: false,
-            timeout: 600000
-    }).done(function () {
-        alert('파일 전송이 완료되었습니다.');
-    }).fail(function (error) {
-        alert(error);
+        url: "/api/va/filesup",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            if (data.result) {
+                alert("Success");
+            }
+            else {
+                alert(data.result);
+            }
+        },
+        err: function (err) {
+            alert(err.status);
+        }
     });
 }
-
-/*
-if (files[0].type.match(/image.*//*    )) {
-
-}else{
-    alert('이미지가 아닙니다.');
-    return;
-}
-*/
-
-
-/*
-        //preventDefault 는 기본으로 정의된 이벤트를 작동하지 못하게 하는 메서드이다. submit을 막음
-        event.preventDefault();
-
-        // Get form
-        var form = $('#fileUploadForm')[0];
-
-	    // Create an FormData object 
-        var data = new FormData(form);
-
-	   // disabled the submit button
-        $("#btnSubmit").prop("disabled", true);
-
-        $.ajax({
-            type: "POST",
-            enctype: 'multipart/form-data',
-            url: "/api/v1/filesup",
-            data: data,
-            processData: false,
-            contentType: false,
-            cache: false,
-            timeout: 600000,
-            success: function (data) {
-            	alert("complete");
-                $("#btnSubmit").prop("disabled", false);
-            },
-            error: function (e) {
-                console.log("ERROR : ", e);
-                $("#btnSubmit").prop("disabled", false);
-                alert("fail");
-            }
-        });
-*/
