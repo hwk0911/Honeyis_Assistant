75c375414842f877bc518d2089d6529340617c4a
hwk0911 <hwk0911@gmail.com>
2020-05-23 15:51:46 +0900

    전체 패키지의 커밋안된 내용 추가

---
+++ Web-Server/build/resources/main/static/js/app/orderFile.js
@@ -0,0 +1,54 @@
+$('.order')
+    .on("dragover", dragOver)
+    .on("dragleave", dragOver)
+    .on("drop", uploadFiles);
+
+function dragOver(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    if (e.type == "dragover") {
+        $(e.target).css({
+            "background-color": "black",
+            "outline-offset": "-20px"
+        });
+    } else {
+        $(e.target).css({
+            "background-color": "gray",
+            "outline-offset": "-10px"
+        });
+    }
+}
+
+function uploadFiles(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    dragOver(e); //1
+
+    e.dataTransfer = e.originalEvent.dataTransfer; //2
+    var files = e.target.files || e.dataTransfer.files;
+
+    var formData = new FormData();
+
+    for (var index = 0, size = files.length; index < size; ++index) {
+        formData.append("files", files[index]);
+    }
+
+    $.ajax({
+        type: "POST",
+        url: "/api/va/orderfilesup",
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function (data) {
+            if (data.result) {
+                alert("Success");
+            }
+            else {
+                alert(data.result);
+            }
+        },
+        err: function (err) {
+            alert(err.status);
+        }
+    });
+}
