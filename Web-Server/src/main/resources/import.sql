de00fa163e3fc6f4a005e3b0d1a33b1383f7758d
hwk0911 <hwk0911@gmail.com>
2020-06-01 19:32:15 +0900

    재고 mustache logic 적용

--- Web-Server/src/main/resources/import.sql
+++ Web-Server/src/main/resources/import.sql
@@ -1,1 +1,2 @@
 INSERT INTO USERS (COMPANY, USER_ID, PASSWORD) VALUES ('cafeCoder', 'hwk0911@gmail.com', '123123');
+INSERT INTO STOCKS (USER_ID, PRODUCT_NAME, COLOR, SIZE, AMOUNT) VALUES ('hwk0911@gmail.com', '(초특가!/1+1할인) 박시핏 무지 긴팔면T', '베이지', 'FREE', '16');
