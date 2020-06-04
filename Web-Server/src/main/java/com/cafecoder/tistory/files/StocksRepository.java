e64f182a2e09fb01f6c51dcdb0d65aa2c22f5835
hwk0911 <hwk0911@gmail.com>
2020-06-04 19:31:11 +0900

    StocksRepository.java
    modify : @Id

--- Web-Server/src/main/java/com/cafecoder/tistory/files/StocksRepository.java
+++ Web-Server/src/main/java/com/cafecoder/tistory/files/StocksRepository.java
@@ -2,7 +2,11 @@ package com.cafecoder.tistory.files;
 
 import com.cafecoder.tistory.user.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface StocksRepository extends JpaRepository<Stocks, String> {
-    Stocks findByUserId(String userId);
+import java.util.List;
+
+public interface StocksRepository extends JpaRepository<Stocks, Long> {
+    Stocks findByUserIdAndProductNameAndColorAndSize(String userId, String productName, String color, String size);
+    List<Stocks> findByUserId(String userId);
 }
