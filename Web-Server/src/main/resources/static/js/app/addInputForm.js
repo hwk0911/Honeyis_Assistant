658fb5a0ac338057a9c63f60be9b1c70c5b70601
hwk0911 <hwk0911@gmail.com>
2020-06-07 18:12:12 +0900

    commit 누락항목 commit

--- Web-Server/src/main/resources/static/js/app/addInputForm.js
+++ Web-Server/src/main/resources/static/js/app/addInputForm.js
@@ -16,10 +16,12 @@ var addForm = {
         var cell2 = row.insertCell(1);
         var cell3 = row.insertCell(2);
         var cell4 = row.insertCell(3);
-        cell1.innerHTML = '<td class="form-group">' + '<input type="text" id="productName" class="form-control" placeholder="productName">' + '</td>';
-        cell2.innerHTML = '<td class="form-group">' + '<input type="text" id="color" class="form-control" placeholder="color">' + '</td>';
-        cell3.innerHTML = '<td class="form-group">' + '<input type="text" id="size" class="form-control" placeholder="size">' + '</td>';
-        cell4.innerHTML = '<td class="form-group">' + '<input type="text" id="amount" class="form-control" placeholder="amount">' + '</td>';
+        var cell5 = row.insertCell(4);
+        cell1.innerHTML = '<td class="form-group">' + '<input type="text" id="client" class="form-control" placeholder="client">' + '</td>';
+        cell2.innerHTML = '<td class="form-group">' + '<input type="text" id="productName" class="form-control" placeholder="productName">' + '</td>';
+        cell3.innerHTML = '<td class="form-group">' + '<input type="text" id="color" class="form-control" placeholder="color">' + '</td>';
+        cell4.innerHTML = '<td class="form-group">' + '<input type="text" id="size" class="form-control" placeholder="size">' + '</td>';
+        cell5.innerHTML = '<td class="form-group">' + '<input type="text" id="amount" class="form-control" placeholder="amount">' + '</td>';
     },
 
     removeInput: function () {
